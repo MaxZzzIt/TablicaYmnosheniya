5ff34b9c57fb52f3df6af29a2cb562dcdebc700b
Maksim Kushner <113466296+MaxZzzIt@users.noreply.github.com>
2022-11-22 22:23:34 +0300

    Вывод таблицы умножения на экран через цикл

---
+++ src/by/home/maxzzzit/TablicaYmnosheniya.java
@@ -0,0 +1,13 @@
+package by.home.maxzzzit;
+
+public class TablicaYmnosheniya {
+	public static void main(String[]args) {
+		int rez;
+		for (int i=1;i<=10;i++) {
+			for (int j=1;j<=10;j++) {
+				System.out.println(i+"*"+j+"="+i*j);
+			}
+			System.out.println();
+		}
+	}
+}
